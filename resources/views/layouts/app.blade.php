fba0041e2ff07f254f11735897a3b43274ef9089
sergio <sergioW>
2017-05-03 12:42:19 -0300

    modifique Nombre de Layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,7 @@
 
         <!-- Branding Image -->
         <a class="navbar-brand" href="{{ url('/') }}">
-          {{ config('app.name', 'Laravel') }}
+          Sistema Avicola
         </a>
       </div>
 
