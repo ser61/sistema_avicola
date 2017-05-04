db6545d7a1b1a633b727530de6c80714427be91b
eghac <eliot.humerez@gmail.com>
2017-05-03 21:01:50 -0400

    Eliot
    Telefono Model

--- app/Telefono.php
+++ app/Telefono.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Telefono extends Model
 {
-    //
+    protected $table ='Telefono';
+
+    protected $primaryKey = 'id';
+
+    public $timestamps=false;
+
+    protected $fillable = [
+        'numero',
+        'idPersona',
+        'visible'
+    ];
+
+    protected $hidden = [
+
+    ];
 }
